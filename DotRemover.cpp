5f8e65bed45d95eab733db192a21765e456a072a
atif9 <mohammadatif92080@gmail.com>
2021-12-11 21:20:30 +0530

    done

---
+++ DotRemover.cpp
@@ -0,0 +1,24 @@
+/* C++ code to remove the starting numbers from file
+*/
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+
+int  main()
+{
+    string line;
+    string::iterator it;
+    ifstream mainFile;
+    mainFile.open("dotFile.txt",ios::in);
+    ofstream newFile;
+    newFile.open("newDotFile.txt",ios::out);
+    while(getline(mainFile,line))
+    {
+        for ( it = line.begin();it!=line.end(),*it!='.';it++);
+        string updatedLine(it+1,line.end());
+        newFile<<updatedLine<<endl;
+
+    }
+    newFile.close();
+    return 0;
+}
